0234b3735f94de7eca42f6d10c2957b8c0ce55c2
sherif6931 <sherif6931@github.com>
2025-11-18 19:51:45 +0200

    linked read/write sig routine

--- incl/_rint.h
+++ incl/_rint.h
@@ -3,18 +3,8 @@
 
 #include <unistd.h>
 
-int buf[2];
-
-int sig_init(void) {
-    return pipe(buf);
-}
-
-ssize_t sig_write(int value) {
-    return write(buf[1], &value, sizeof(value));
-}
-
-ssize_t sig_read(int *value) {
-    return read(buf[0], value, sizeof(*value));
-}
+int sig_init(void);
+ssize_t sig_write(int value);
+ssize_t sig_read(int *value);
 
 #endif
